f541dd1a98ab6f65637af44b3e16b576df5b5c07
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-02-02 02:01:01 +0100

    Generated multiple documents

--- src/external_service.ts
+++ src/external_service.ts
@@ -1,5 +1,5 @@
-export async function* getDocuments(): AsyncGenerator<Documents> {
-  const PER_PAGE = 2;
+export async function* getDocuments(): AsyncGenerator<Document> {
+  const PER_PAGE = 10;
   const PAGE = 1;
 
   let response = await getDocmuentPaged(PER_PAGE, PAGE);
@@ -7,6 +7,31 @@ export async function* getDocuments(): AsyncGenerator<Documents> {
   for (let document of response) {
     yield document;
   }
+}
+
+export class Document {
+  name: string;
+  version = [
+    { version: "V1", role: "A" },
+    { version: "V1", role: "A" },
+    { version: "V2", role: "A" },
+    { version: "V", role: "A" },
+  ];
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+function generateMultipleDocuments(count: number): Document[] {
+  let documents: Document[] = [];
+
+  for (let i = 1; i <= count; i++) {
+    let documentName = `BREEZ-MJT-GH-B1-DR-A-0${i}.pdf`;
+    documents.push(new Document(documentName));
+  }
+
+  return documents;
 }
 
 export type Version = Map<string, string>;
@@ -19,39 +44,8 @@ export type Documents = Array<Version>;
 async function getDocmuentPaged(
   perPage: number,
   page: number
-): Promise<Array<Documents>> {
-  let attribute1 = new Map<string, string>();
-  let attribute2 = new Map<string, string>();
-  let attribute3 = new Map<string, string>();
-  let attribute4 = new Map<string, string>();
-
-  attribute1.set("name", "BREEZ-MJT-GH-B1-DR-A-0001.pdf");
-  attribute1.set("role", "A");
-  attribute1.set("version", "V1");
-
-  attribute2.set("name", "BREEZ-MJT-GH-GF-DR-A-0002.pdf");
-  attribute2.set("role", "A");
-  attribute2.set("version", "V2");
-
-  attribute3.set("name", "BREEZ-MJT-ZZ-01-DR-0003.pdf");
-  attribute3.set("role", "A");
-  attribute3.set("version", "V1");
-
-  attribute4.set("name", "BREEZ-MJT-ZZ-01-DR-0004.pdf");
-  attribute4.set("role", "A");
-  attribute4.set("version", "V1");
-
-  let version1: Version = attribute1;
-  let version2: Version = attribute2;
-  let version3: Version = attribute3;
-  let version4: Version = attribute4;
-
-  let document1: Documents = [version1];
-  let document2: Documents = [version2];
-  let document3: Documents = [version3];
-  let document4: Documents = [version4];
-
-  let data = [document1, document2, document3, document4];
+): Promise<Array<Document>> {
+  let data = generateMultipleDocuments(100);
 
   // I get the total number of the page by dividing
   // the length of the data by perPage
